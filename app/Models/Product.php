c6224bb36da7218326f0fbde79d33a479dd2b3cb
fadd00 <fadd00@users.noreply.github.com>
2026-03-07 10:46:03 +0700

    Add tugas 3 documentation for kelas E by Andhika Prima Hutama

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'qty',
+        'price',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+}
---
+++ tugas/tugas3.md
@@ -0,0 +1,14 @@
+# tugas 3 PWF
+
+## kelas E
+
+### Andhika Prima Hutama - 20230140239
+
+### migration
+![alt text](image3/image.png)
+
+### database
+![alt text](image3/image-1.png)
+
+### model
+![alt text](image3/image-2.png)
